b479f592d4130fdb89b3c22675e1350b73c28592
noa-no-h <noa@noanoa.space>
2024-02-04 12:08:27 -0600

    porchlight

---
+++ PorchlightThirdMillenniumThinking/assets/store/cart.js
@@ -0,0 +1,1 @@
+(function(){$(function(){return $("form#update-cart").is("*")&&$("form#update-cart a.delete").show().one("click",function(){return $(this).parents(".line-item").first().find("input.line_item_quantity").val(0),$(this).parents("form").first().submit(),!1}),$("form#update-cart").submit(function(){return $("form#update-cart #update-button").attr("disabled",!0)})})}).call(this);
